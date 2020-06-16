d9d591a530155b2976e72c133a088d5461b06d97
collin9726 <owino.collin@gmail.com>
2020-06-16 16:58:16 +0300

    create team form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,SelectField
-from wtforms.validators import Required, Email, Length
+from wtforms import StringField,TextAreaField,SelectField,SubmitField,ValidationError
+from wtforms.validators import Required,Email,Length
+import email_validator
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+
+class TeamForm(FlaskForm):
+    team_name = StringField('Team name',validators=[Required()])    
+    category = SelectField('Category', validators=[Required()], choices=[('football','Football'),('hockey','Hockey'),('rugby','Rugby'),('cricket','Cricket'),('basketball','Basketball')])
+    submit = SubmitField('Create team')
+    
