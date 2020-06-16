f2cdc65b79d7e91c01fe42bd290987036e796fd9
Cecilia Barasa <cecibarasa@gmail.com>
2020-06-16 11:06:59 +0300

    add user profile

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nabalayo:karitie@localhost/sports'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = '1738'
 
 
 class ProdConfig(Config):
